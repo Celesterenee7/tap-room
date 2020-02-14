c06a55bd4dcedeff28c264b5e1199a3a510e4bef
Celeste Morton <creneemorton@gmail.com>
2020-02-14 09:49:35 -0800

    add beer logo as favicon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
 
     return (
-        <div className="Body">
+        <div>
             <div className="row">
                 <div className="col-md-12">
 
