ddc530190183a13ddbf65201d8a8bf2ae39e149f
Celeste Morton <creneemorton@gmail.com>
2020-02-14 09:01:05 -0800

    add header and container to app.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
-  return ( <
-    div className = "App" >
-    <
-    header className = "App-header" >
-    <
-    p >
-    Edit < code > src / App.js < /code> and save to reload. < /
-    p > <
-    a className = "App-link"
-    href = "https://reactjs.org"
-    target = "_blank"
-    rel = "noopener noreferrer" >
-    Learn React <
-    /a> < /
-    header > <
-    /div>
+  return (
+    <div>
+      <Header />
+      <div className="container">
+
+      </div>
+    </div>
   );
 }
+
+
 
 export default App;
